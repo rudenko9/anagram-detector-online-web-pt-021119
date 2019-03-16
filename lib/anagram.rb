1eb02774c037ae770827c68e5852458555c365c5
Nataliya R <n.rudenko@gmail.com>
2019-03-16 01:25:55 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,6 @@
 # Your code goes here!
 class Anagram
   
-  def initialize(anagram)
-    @anagram = anagram 
-  end 
   
-def match(words)
-  words.select do |word|
-    is_anagram?(word)
- end    
- end 
   
 end 
