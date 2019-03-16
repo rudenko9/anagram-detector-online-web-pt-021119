cdb2dfe1935209b62ff24ed68cf18651c4eab740
Nataliya R <n.rudenko@gmail.com>
2019-03-16 01:36:58 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+   array.select do |element|
+     @word.split("").sort == element.split("").sort
+  end
+end 
+ 
+end
+  
